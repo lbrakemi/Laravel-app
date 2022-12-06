98c51877d13f84cd42cde00f2d692c87efb8f60b
lbrakemi <lbrakemi88@gmail.com>
2022-12-05 22:49:28 -1100

    Basic Laravel

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+ <!-- Page Header-->
+ <header class="masthead" style="background-image: url({{asset('assets/img/home-bg.jpg')}})">
+    <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="site-heading">
+                    <h1>Clean Blog</h1>
+                    <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+
+    <div class="col-md-6 mx-auto my-5">
+        <div class="card">
+            <div class="card-body shadow">
+
+                <form action="{{route('posts.store')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group mb-3 ">
+                        <label for="title">Title</label>
+                        <input type="text" name="title" id="title" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="body">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="image">Image</label>
+                       <input type="file" name="image" id="image" class="form-control">
+                    </div>
+
+                    <button type="submit" class="btn btn-outline-primary w-100">Create Post</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', [FrontController::class, 'index']);
+
+Route::get('about', [FrontController::class, 'about'] );
+Route::get('contact', [FrontController::class, 'contact']);
+Route::get('posts', [PostController::class, 'index']);
+Route::resource('posts', PostController::class);
